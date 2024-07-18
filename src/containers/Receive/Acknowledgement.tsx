fb81f579a99829178a254ab65b217d42d1162c70
EkfrazoMohammed <mohammed@ekfrazo.in>
2024-07-18 13:33:43 +0530

    updated decimal

--- src/containers/Receive/Acknowledgement.tsx
+++ src/containers/Receive/Acknowledgement.tsx
@@ -514,9 +514,9 @@ const Acknowledgement = ({ onData, showTabs, setShowTabs }) => {
                 render: (_, record: unknown) => (
                     <p>
                         {record.balance > 0 ?
-                            <span style={{ color: "#009f23", fontWeight: "600" }}>+ {(record.balance)}</span>
+                            <span style={{ color: "#009f23", fontWeight: "600" }}>+ {parseFloat((record.balance)).toFixed(2)}</span>
                             :
-                            <span style={{ color: "red" }}>{(record.balance)}</span>
+                            <span style={{ color: "red" }}>{parseFloat((record.balance)).toFixed(2)}</span>
 
                         }
                     </p>
